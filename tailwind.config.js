dbce4a26073cee523cd9921f2807412757b2d859
Muhammad Dafa Ardiansyah <71364076+mdafaardiansyah@users.noreply.github.com>
2022-08-26 08:59:42 +0700

    Initiating Sidica Project with create-react-app ReactJS and Tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      white: "#ffffff",
+      white2: "#f8f8f8",
+      blue: "#277bc0",
+      orange: "#ffb200",
+      yellow: "#ffcb42",
+      cream: "#fff4cf",
+    },
+  },
+  plugins: [],
+}
